7158eb5d7da9efae03a1e74e551997bdf3e2f01f
ringechchen <ringechchen@tencent.com>
2017-06-15 15:08:23 +0800

    merge the compressed mode

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,16 +12,11 @@ $(document).ready(function(){
 			$("#private_key_hex").val("");
 			$("#private_address_valid").val("");
 			$("#public_key_hex").val("");
-			$("#public_address_hex_nocomm").val("");
-			$("#public_address_hex_comm").val("");
-			$("#public_address_sha_nocomm").val("");
-			$("#public_address_sha_comm").val("");
-			$("#public_address_emd_nocomm").val("");
-			$("#public_address_emd_comm").val("");
-			$("#public_address_chk_nocomm").val("");
-			$("#public_address_chk_comm").val("");
-			$("#public_address_nocomm").val("");
-			$("#public_address_comm").val("");
+			$("#public_address_hex").val("");
+			$("#public_address_sha").val("");
+			$("#public_address_emd").val("");
+			$("#public_address_chk").val("");
+			$("#public_address").val("");
 			//private_address - private_address_hex
 			var private_address     = $("#private_address").val();
 			var private_address_hex = bs58.decode(private_address);
@@ -49,42 +44,37 @@ $(document).ready(function(){
 			//private_key_hex - public_key_hex
 			var public_key_hex = elliptic.keyFromPrivate(private_key_hex).getPublic('hex');
 			$("#public_key_hex").val(public_key_hex);
-			//public_key_hex - (public_address_hex_nocomm + public_address_hex_comm)
+			//public_key_hex - public_address_hex
 			var public_address_hex_x = public_key_hex.substring(2, 66);
 			var public_address_hex_y = public_key_hex.substring(66, 130);
-			var public_address_hex_nocomm = public_key_hex;
-			$("#public_address_hex_nocomm").val(public_key_hex);
-			var public_address_hex_comm = '';
+			var public_address_hex = '';
 			var public_address_hex_y_chk = public_address_hex_y.split("").pop();
-			if ($.inArray(public_address_hex_y_chk, ['0', '2', '4', '6', '8', 'a', 'c', 'e']) != -1) {
-				public_address_hex_comm = "02" + public_address_hex_x;
-				$("#public_address_hex_comm").val(public_address_hex_comm);
+			var private_address_valid = $("#private_address_valid").val();
+			if (private_address_valid == 'normal format') {
+				public_address_hex = public_key_hex;
+			} else if (private_address_valid == 'compressed format') {
+				if ($.inArray(public_address_hex_y_chk, ['0', '2', '4', '6', '8', 'a', 'c', 'e']) != -1) {
+					public_address_hex = "02" + public_address_hex_x;
+				} else {
+					public_address_hex = "03" + public_address_hex_x;
+				}
 			} else {
-				public_address_hex_comm = "03" + public_address_hex_x;
-				$("#public_address_hex_comm").val(public_address_hex_comm);
+				public_address_hex = '';
 			}
-			//(public_address_hex_nocomm + public_address_hex_comm) - (public_address_sha_nocomm + public_address_sha_comm)
-			var public_address_sha_nocomm = sha256(conv(public_address_hex_nocomm, { in:'hex' }));
-			$("#public_address_sha_nocomm").val(public_address_sha_nocomm);
-			var public_address_sha_comm = sha256(conv(public_address_hex_comm, { in:'hex' }));
-			$("#public_address_sha_comm").val(public_address_sha_comm);
-			//(public_address_sha_nocomm + public_address_sha_comm) - (public_address_emd_nocomm + public_address_emd_comm)
-			var public_address_emd_nocomm = new RIPEMD160().update(conv(public_address_sha_nocomm, { in:'hex' })).digest('hex');
-			$("#public_address_emd_nocomm").val(public_address_emd_nocomm);
-			var public_address_emd_comm = new RIPEMD160().update(conv(public_address_sha_comm, { in:'hex' })).digest('hex');
-			$("#public_address_emd_comm").val(public_address_emd_comm);
-			//(public_address_emd_nocomm + public_address_emd_comm) - (public_address_chk_nocomm + public_address_chk_comm)
-			var public_address_chk_nocomm = sha256(conv(sha256(conv("00"+public_address_emd_nocomm, { in:'hex' })), { in:'hex' }));
-			public_address_chk_nocomm = public_address_chk_nocomm.substring(0, 8);
-			$("#public_address_chk_nocomm").val(public_address_chk_nocomm);
-			var public_address_chk_comm = sha256(conv(sha256(conv("00"+public_address_emd_comm, { in:'hex' })), { in:'hex' }));
-			public_address_chk_comm = public_address_chk_comm.substring(0, 8);
-			$("#public_address_chk_comm").val(public_address_chk_comm);
-			//(public_address_nocomm + public_address_comm)
-			var public_address_nocomm = bs58.encode(new Buffer("00"+public_address_emd_nocomm+public_address_chk_nocomm, 'hex'))
-			$("#public_address_nocomm").val(public_address_nocomm);
-			var public_address_comm = bs58.encode(new Buffer("00"+public_address_emd_comm+public_address_chk_comm, 'hex'))
-			$("#public_address_comm").val(public_address_comm);
+			$("#public_address_hex").val(public_address_hex);
+			//public_address_hex - public_address_sha
+			var public_address_sha = sha256(conv(public_address_hex, { in:'hex' }));
+			$("#public_address_sha").val(public_address_sha);
+			//public_address_sha - public_address_emd
+			var public_address_emd = new RIPEMD160().update(conv(public_address_sha, { in:'hex' })).digest('hex');
+			$("#public_address_emd").val(public_address_emd);
+			//public_address_emd - public_address_chk
+			var public_address_chk = sha256(conv(sha256(conv("00"+public_address_emd, { in:'hex' })), { in:'hex' }));
+			public_address_chk = public_address_chk.substring(0, 8);
+			$("#public_address_chk").val(public_address_chk);
+			//public_address
+			var public_address = bs58.encode(new Buffer("00"+public_address_emd+public_address_chk, 'hex'))
+			$("#public_address").val(public_address);
 		} catch(err) {
 			$("#errmsg").val(err.message);
 		}
